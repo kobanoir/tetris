20f558e0b0d5c8e5f76f7111dee12da5e555eed0
kobanoir <koba.kosen@gmail.com>
2017-05-22 20:36:37 +0900

    add mino

--- tetris/tetris/Block.cpp
+++ tetris/tetris/Block.cpp
@@ -1,9 +1,43 @@
 #include "Block.h"
 
 Block::Block() {
-	make_block();
+	make_mino();
 }
 
-void Block::make_block() {
-
+void Block::make_mino() {
+	//棒
+	tetris[2][0] = 1;
+	tetris[2][1] = 1;
+	tetris[2][2] = 1;
+	tetris[2][3] = 1;
+	//四角形
+	square[0][0] = 1;
+	square[0][1] = 1;
+	square[1][0] = 1;
+	square[1][1] = 1;
+	//階段A
+	stairA[1][0] = 1;
+	stairA[2][0] = 1;
+	stairA[0][1] = 1;
+	stairA[1][1] = 1;
+	//階段B
+	stairB[0][0] = 1;
+	stairB[1][0] = 1;
+	stairB[1][1] = 1;
+	stairB[2][1] = 1;
+	//鉤A
+	hookA[0][0] = 1;
+	hookA[0][1] = 1;
+	hookA[1][1] = 1;
+	hookA[2][1] = 1;
+	//鉤B
+	hookB[2][0] = 1;
+	hookB[0][1] = 1;
+	hookB[1][1] = 1;
+	hookB[2][1] = 1;
+	//T字ブロック
+	tletter[1][0] = 1;
+	tletter[0][1] = 1;
+	tletter[1][1] = 1;
+	tletter[2][1] = 1;
 }
--- tetris/tetris/Block.h
+++ tetris/tetris/Block.h
@@ -1,13 +1,20 @@
 #pragma once
 #include <Siv3D.hpp>
+#include <vector>
+using namespace std;
 
 class Block {
 public:
 	Block();
-
-	void make_block();
+	
+	void make_mino();
 
 private:
-	//4*2‚Ìƒ~ƒm‚ð7ŒÂŠi”[‚Å‚«‚é
-	int block[4][2][7];
+	int tetris[4][4];
+	int square[2][2];
+	int stairA[3][3];
+	int stairB[3][3];
+	int hookA[3][3];
+	int hookB[3][3];
+	int tletter[3][3];
 };
